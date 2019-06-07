740ceac446abbd5fc20a82f00733a782116b0769
Fernando May <fmay130@gmail.com>
2019-06-07 01:44:02 -0500

    b

---
+++ app.js
@@ -0,0 +1,26 @@
+//const potencia=document.querySelector('#potencia');
+//potencia.addEventListener('keyup',()=>console.log(potencia.value));
+
+var app=new Vue({
+    el: '#skinner',
+    data: {
+        potencia: '',
+        velmotriz: '',
+        velconducida: '',
+        trabajomotriz: '',
+        trabajoconducida: '',
+        pasodiametral: '',
+        dientes: '',
+        jp: '',
+        jg: '',
+        i: '',
+        tipoengrane: '',
+        confiabilidad: '',
+        horasdia: '',
+        diasaño: '',
+        años: '',
+        recta1: '',
+        recta2: ''
+
+    }
+});
